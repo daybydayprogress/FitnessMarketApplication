befd75e6858b50c3d77e1900cf9fc5d8caf8c357
daybydayprogress <usic.natan@gmail.com>
2021-07-20 23:15:50 +0200

    initial commit

---
+++ src/main/java/natan/fitnessmarket/FitnessMarketApplication.java
@@ -0,0 +1,13 @@
+package natan.fitnessmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/natan/fitnessmarket/FitnessMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package natan.fitnessmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
